09eeb894bc8c305f4f2e2c3b782cc5a83b9f2b41
ollenat <54154070+Ollenat@users.noreply.github.com>
2024-12-01 15:06:59 +0100

    third try: fixed i+1 in qsort

--- 01/main.c
+++ 01/main.c
@@ -5,13 +5,14 @@
 #define bufferSize 256
 
 // Comparison function
-int compare(const void* a, const void* b) {
-   return (*(int*)a - *(int*)b);
+int compare(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
 }
 
 int main(int argc, char const *argv[])
 {
-    
+
     char buffer[bufferSize];
 
     int *list1 = NULL;
@@ -22,37 +23,41 @@ int main(int argc, char const *argv[])
 
     unsigned long long int total = 0;
 
-    while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
-        //Buffer is now a line from the input
+    while (fgets(buffer, sizeof(buffer), stdin) != NULL)
+    {
+        // Buffer is now a line from the input
         char *next_token = NULL;
         char *first = strtok_s(buffer, " ", &next_token);
         char *second = strtok_s(NULL, " ", &next_token);
 
-        //we have the first and the second numbers now, as strings.
-        //Parse them
+        // we have the first and the second numbers now, as strings.
+        // Parse them
         int num1 = atoi(first);
         int num2 = atoi(second);
-        if (i == capacity) {
+        if (i == capacity)
+        {
             capacity = (capacity == 0) ? 1 : capacity * 2;
             list1 = realloc(list1, capacity * sizeof(int));
             list2 = realloc(list2, capacity * sizeof(int));
         }
-        
+        printf("%d, %d\n", num1, num2);
         list1[i] = num1;
         list2[i] = num2;
 
         i++;
     }
 
-    qsort(list1, i + 1, sizeof(int), compare);
-    qsort(list2, i + 1, sizeof(int), compare);
+    qsort(list1, i, sizeof(int), compare);
+    qsort(list2, i, sizeof(int), compare);
 
     for (int j = 0; j < i; j++)
     {
         int num1 = list1[j];
         int num2 = list2[j];
+        printf("%d, %d\n", num1, num2);
 
-        int diff = max(num1, num2) -  min(num1, num2);
+        int diff = max(num1, num2) - min(num1, num2);
+        printf("%i\n", diff);
         total += diff;
     }
     printf("Total difference:\n%llu", total);
